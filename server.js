b4a0660d688208717dad5b60a0aff7890cfb5898
mobilepig <mobilepig@naver.com>
2024-02-14 20:49:36 +0900

    test

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const app = express();
 dotenv.config();
 const port = 3000;
 
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://www.daddygo.vacations");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
